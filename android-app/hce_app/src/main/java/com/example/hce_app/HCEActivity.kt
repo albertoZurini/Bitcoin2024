feb5930524dc4d332650d0d063a5a137ec16a804
Alberto Zurini <alberto.zurini@gmail.com>
2024-11-11 21:19:03 +0100

    feat: fixed app ui

--- android-app/hce_app/src/main/java/com/example/hce_app/HCEActivity.kt
+++ android-app/hce_app/src/main/java/com/example/hce_app/HCEActivity.kt
@@ -10,16 +10,30 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.hce_app.cardEmulation.KHostApduService
 
 class HCEActivity : ComponentActivity() {
@@ -41,6 +55,7 @@ class HCEActivity : ComponentActivity() {
             val data = b!!.getString("data").toString()
             if (data.length > 0) {
                 if (data != null) {
+                    nfcMessage = data;
                     setNFCMessage(data)
                 }
             }
@@ -55,8 +70,55 @@ class HCEActivity : ComponentActivity() {
                     Text(text = "Can't get NFCAdapter")
                 }
             }
-        }
+        } else {
+            setContent {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            // Display NFC icon
+                            Icon(
+                                painter = painterResource(id = R.drawable.nfc), // Replace with your NFC icon resource
+                                contentDescription = "NFC Icon",
+                                modifier = Modifier.size(128.dp), // Adjust size as needed
+                                tint = Color.Gray // Choose a color as per your theme
+                            )
 
+                            Spacer(modifier = Modifier.height(16.dp)) // Space between icon and text
+
+                            // Display text "BTC Receiver"
+                            Text(
+                                text = "BTC\nTransmitter",
+                                textAlign = TextAlign.Center,
+                                fontSize = 36.sp,
+                                fontWeight = FontWeight.ExtraBold,
+                                color = Color.Gray
+                            )
+
+                            TextField(
+                                value = nfcMessage,
+                                onValueChange = { nfcMessage = it },
+                                label = { Text("message") }
+                            )
+
+                            Button(
+                                onClick={
+                                    setNFCMessage()
+                                }
+                            ) {
+                                Text("Set the message")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+/*
         setContent {
             Surface(modifier = Modifier.fillMaxSize()) {
                 Column {
@@ -88,7 +150,7 @@ class HCEActivity : ComponentActivity() {
                 }
                 // TODO: Shazam should add here the GUI information for when the transaction has been made as well as for WalletConnect
             }
-        }
+        }*/
     }
 
     private fun supportNfcHceFeature() =
@@ -107,7 +169,7 @@ class HCEActivity : ComponentActivity() {
         var urlToCast: String = ""
 
         if(message.length == 0) {
-            urlToCast = "web+stellar:$address?amount=$amount";
+            urlToCast = nfcMessage; //"web+stellar:$address?amount=$amount";
         } else {
             urlToCast = message
         }
--- android-app/hce_app/src/main/java/com/example/hce_app/NFCReaderActivity.kt
+++ android-app/hce_app/src/main/java/com/example/hce_app/NFCReaderActivity.kt
@@ -11,15 +11,25 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hce_app.parser.NDEFTools
 import java.io.IOException
@@ -53,6 +63,42 @@ class NFCReaderActivity : ComponentActivity() {
                 }
             }
         } else {
+
+            setContent {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            // Display NFC icon
+                            Icon(
+                                painter = painterResource(id = R.drawable.nfc), // Replace with your NFC icon resource
+                                contentDescription = "NFC Icon",
+                                modifier = Modifier.size(128.dp), // Adjust size as needed
+                                tint = Color.Gray // Choose a color as per your theme
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp)) // Space between icon and text
+
+                            // Display text "BTC Receiver"
+                            Text(
+                                text = "BTC\nReceiver",
+                                textAlign = TextAlign.Center,
+                                fontSize = 36.sp,
+                                fontWeight = FontWeight.ExtraBold,
+                                color = Color.Gray
+                            )
+                        }
+                    }
+                }
+            }
+
+
+            /*
             setContent {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     Box(
@@ -69,6 +115,7 @@ class NFCReaderActivity : ComponentActivity() {
                     }
                 }
             }
+            */
         }
     }
 
