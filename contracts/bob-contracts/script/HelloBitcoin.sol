1690cc679731f63235594622dc0285000bffeb41
rex <133902786+0xr3x@users.noreply.github.com>
2024-11-12 05:52:54 +0700

    feat: update deploy script

--- contracts/bob-contracts/script/HelloBitcoin.sol
+++ contracts/bob-contracts/script/HelloBitcoin.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 import {Script, console2} from "forge-std/Script.sol";
 import {TestLightRelay} from "@bob-collective/bob/relay/TestLightRelay.sol";
 import {HelloBitcoin} from "../src/HelloBitcoin.sol";
+import {BOBPointOfSale} from "../src/BOBPointOfSale.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract HelloWorldScript is Script {
     function setUp() public {}
@@ -16,7 +18,9 @@ contract HelloWorldScript is Script {
 
         TestLightRelay relay = new TestLightRelay();
 
-        new HelloBitcoin(relay, usdt);
+        HelloBitcoin helloBitcoin = new HelloBitcoin(relay, usdt);
+
+        new BOBPointOfSale(address(helloBitcoin), IERC20(usdt), address(this));
 
         vm.stopBroadcast();
     }
